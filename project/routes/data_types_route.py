ebeeb448969c22ab41ef0bad226527a1fbb955df
jalvarezga <joalgalna@gmail.com>
2025-07-01 17:52:30 -0600

    Revert "changes in data types that I need to undo"
    This reverts commit 43a6e69cd883e911fd0aa371b54f2fed58503929.

--- project/routes/data_types_route.py
+++ project/routes/data_types_route.py
@@ -1,28 +1,18 @@
-from flask import Blueprint, render_template, request
-from utils.helpers import read_csv
-import os
+from flask import Blueprint, request, jsonify
 import pandas as pd
+import os
 from config import UPLOAD_FOLDER
+from utils.helpers import read_csv
 
 data_types = Blueprint('data_types', __name__)
 
-@data_types.route('/show_data_types', methods=['POST'])
-def show_data_types():
-    file_path = request.form['file_path']
+@data_types.route('/get_data_types', methods=['POST'])
+def get_data_types():
+    data = request.get_json()
+    file_path = data.get('file_path')
+    df = read_csv(os.path.join(UPLOAD_FOLDER, file_path))
 
-    # Load DataFrame from uploaded CSV
-    full_path = os.path.join(UPLOAD_FOLDER, file_path)
-    df = read_csv(full_path)
-
-    # Gather columns
-    numeric_columns = df.select_dtypes(include=['number']).columns.tolist()
-    all_columns = df.columns.tolist()
-    no_numeric = len(numeric_columns) == 0
-
-    # Compute first rows for possible reuse
-    first_rows = df.head().to_html(classes='table', index=False)
-
-    # Infer and format data types in user-friendly way
+    # Build data type info
     data_types_info = []
     for col in df.columns:
         dtype = df[col].dtype
@@ -40,21 +30,9 @@ def show_data_types():
         else:
             inferred_type = str(dtype)
 
-        data_types_info.append({
-            'column_name': col,
-            'data_type': inferred_type
-        })
+        data_types_info.append({'column_name': col, 'data_type': inferred_type})
 
-    # Turn into HTML table
     data_types_df = pd.DataFrame(data_types_info)
     data_types_html = data_types_df.to_html(classes='table table-striped', index=False)
 
-    return render_template('index.html',
-                           success=True,
-                           filename=file_path,
-                           column_names=numeric_columns,
-                           all_column_names=all_columns,
-                           first_rows=first_rows,
-                           data_types_html=data_types_html,
-                           no_numeric=no_numeric,
-                           current_tab='data-types-section')
+    return jsonify({'html': data_types_html})
