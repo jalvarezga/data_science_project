82f8281edac98614e0d68414d3483b8ac7ea19cb
jalvarezga <joalgalna@gmail.com>
2025-07-01 17:37:16 -0600

    need to fix

--- project/app.py
+++ project/app.py
@@ -4,8 +4,7 @@ from routes.summary_route import summary
 from routes.histogram_route import histogram
 from routes.scatter_route import scatter
 from routes.summary_route import summary
-from routes.data_types_route import data_types
-from routes.show_rows_route import show_rows
+from routes.data_types_route import data_types # <--- NEW
 import matplotlib
 
 matplotlib.use('Agg')  # No GUI for matplotlib
@@ -16,7 +15,6 @@ app.register_blueprint(main)
 app.register_blueprint(histogram)
 app.register_blueprint(scatter)
 app.register_blueprint(summary)
-app.register_blueprint(data_types)
-app.register_blueprint(show_rows)
+app.register_blueprint(data_types) # <--- NEW
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
